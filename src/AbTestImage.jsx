42fc1dca2ce97826859ece91f46803b9f517d79e
millalinden <milla.linden@hyperisland.se>
2024-02-19 21:59:45 +0100

    test

--- src/AbTestImage.jsx
+++ src/AbTestImage.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
 export default function ABTestImage(){
-  const publicUrl = process.env.PUBLIC_URL;
-
   return (
     <section>
       <img
-        src={`${publicUrl}/assets/dachshund-1519374_640.jpg`}
+        src="src/assets/dachshund-1519374_640.jpg"
         alt="Dachshund puppy with blue collar"
         width={400}
         height={300}
       />
       <img
-        src="assets/puppy-1047521_640.jpg"
+        src="src/assets/puppy-1047521_640.jpg"
         alt="Lightbrown puppy"
         width={500}
         height={300}
