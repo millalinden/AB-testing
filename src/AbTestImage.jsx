e50c159527eaa5f541660280f4f18c2bfb73ce5c
millalinden <milla.linden@hyperisland.se>
2024-02-19 21:49:12 +0100

    images

--- src/AbTestImage.jsx
+++ src/AbTestImage.jsx
@@ -4,13 +4,13 @@ const ABTestImage = () => {
   return (
     <section>
       <img
-        src="/assets/dachshund-1519374_640.jpg"
+        src="assets/dachshund-1519374_640.jpg"
         alt="Dachshund puppy with blue collar"
         width={400}
         height={300}
       />
       <img
-        src="/assets/puppy-1047521_640.jpg"
+        src="assets/puppy-1047521_640.jpg"
         alt="Lightbrown puppy"
         width={500}
         height={300}
