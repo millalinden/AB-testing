919535ba57bb797d899816430122f89df9be4b15
millalinden <milla.linden@hyperisland.se>
2024-02-20 09:28:49 +0100

    test

--- src/AbTestImage.jsx
+++ src/AbTestImage.jsx
@@ -3,19 +3,19 @@ import React from "react";
 export default function ABTestImage(){
   return (
     <section>
-    <img
-      src={`${process.env.PUBLIC_URL}/assets/dachshund-1519374_640.jpg`}
-      alt="Dachshund puppy with blue collar"
-      width={400}
-      height={300}
-    />
-    <img
-      src={`${process.env.PUBLIC_URL}/assets/puppy-1047521_640.jpg`}
-      alt="Lightbrown puppy"
-      width={500}
-      height={300}
-    />
-  </section>
+      <img
+        src="/assets/dachshund-1519374_640.jpg"
+        alt="Dachshund puppy with blue collar"
+        width={400}
+        height={300}
+      />
+      <img
+        src="/assets/puppy-1047521_640.jpg"
+        alt="Lightbrown puppy"
+        width={500}
+        height={300}
+      />
+    </section>
   );
 };
 
